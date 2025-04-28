03fbd9f9a4c53c14ee2c09be2037b7b16b9bd070
ningia <113545072+ningia92@users.noreply.github.com>
2025-04-28 13:25:15 +0200

    feat: add a button that will send user a popup asking for the number of squares per side for the new grid

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 const container = document.querySelector('.container')
+
+const createGrid = size => {
+  grid.innerHTML = ''
+  const squareSize = 500 / size
+
+  for (let i = 0; i < size; i++) {
+    const row = document.createElement('div')
+    row.classList.add('row')
+    grid.appendChild(row)
+
+    for (let i = 0; i < size; i++) {
+      const square = document.createElement('div')
+      square.classList.add('square')
+      square.style.width = `${squareSize}px`
+      square.style.height = `${squareSize}px`
+      row.appendChild(square)
+    }
+  }
+
+  const squares = document.querySelectorAll('.square')
+  squares.forEach(square => square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'darkslategray'
+  }))
+}
+
+const createButton = () => {
+  const button = document.createElement('button')
+  button.textContent = 'Grid size'
+  container.appendChild(button)
+  button.addEventListener('click', () => {
+    size = parseInt(prompt('Choose grid size:'))
+    if (size > 100) {
+      alert('Size cannot exceed 100')
+    } else {
+      createGrid(size)
+    }
+  })
+}
+
+createButton()
 const grid = document.createElement('div')
 grid.classList.add('grid')
 container.appendChild(grid)
-const size = 16
-
-for (let i = 0; i < size; i++) {
-  const row = document.createElement('div')
-  row.classList.add('row')
-  grid.appendChild(row)
-  for (let i = 0; i < size; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
-    row.appendChild(square)
-  }
-}
-
-const squares = document.querySelectorAll('.square')
-squares.forEach(square => square.addEventListener('mouseover', () => {
-  square.setAttribute('style', 'background-color: darkslategray')
-}))
+// default size
+let size = 16
+createGrid(size)
 
 
